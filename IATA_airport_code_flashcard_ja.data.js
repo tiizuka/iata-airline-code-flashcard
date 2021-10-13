3649558c5a71b394a5d0cfbe531f60db5558982e
tiizuka <ti1768@b.thyme.jp>
2021-10-13 22:09:49 +0900

    Add airport code

---
+++ IATA_airport_code_flashcard_ja.data.js
@@ -0,0 +1,727 @@
+const IATA_airport_code_ja = [
+  {
+    "code": "ADL",
+    "name": "アデレード",
+    "country": "オーストラリア"
+  },
+  {
+    "code": "AKL",
+    "name": "オークランド",
+    "country": "ニュージーランド"
+  },
+  {
+    "code": "AMS",
+    "name": "アムステルダム",
+    "country": "オランダ"
+  },
+  {
+    "code": "ANC",
+    "name": "アンカレッジ",
+    "country": "アメリカ"
+  },
+  {
+    "code": "ANK",
+    "name": "アンカラ",
+    "country": "トルコ"
+  },
+  {
+    "code": "ATH",
+    "name": "アテネ",
+    "country": "ギリシャ"
+  },
+  {
+    "code": "ATL",
+    "name": "アトランタ",
+    "country": "アメリカ"
+  },
+  {
+    "code": "BCN",
+    "name": "バルセロナ",
+    "country": "スペイン"
+  },
+  {
+    "code": "BER",
+    "name": "ベルリン",
+    "country": "ドイツ"
+  },
+  {
+    "code": "BJS",
+    "name": "北京",
+    "country": "中国"
+  },
+  {
+    "code": "PEK",
+    "name": "北京首都国際空港",
+    "country": "北京(中国)"
+  },
+  {
+    "code": "PKX",
+    "name": "北京大興国際空港",
+    "country": "北京(中国)"
+  },
+  {
+    "code": "BKK",
+    "name": "バンコク",
+    "country": "タイ"
+  },
+  {
+    "code": "BNE",
+    "name": "ブリスベン",
+    "country": "オーストラリア"
+  },
+  {
+    "code": "BOM",
+    "name": "ムンバイ",
+    "country": "インド"
+  },
+  {
+    "code": "BOS",
+    "name": "ボストン",
+    "country": "アメリカ"
+  },
+  {
+    "code": "BRU",
+    "name": "ブリュッセル",
+    "country": "ベルギー"
+  },
+  {
+    "code": "BUD",
+    "name": "ブダペスト",
+    "country": "ハンガリー"
+  },
+  {
+    "code": "CAI",
+    "name": "カイロ",
+    "country": "エジプト"
+  },
+  {
+    "code": "CAN",
+    "name": "広州",
+    "country": "中国"
+  },
+  {
+    "code": "CBR",
+    "name": "キャンベラ",
+    "country": "オーストラリア"
+  },
+  {
+    "code": "CHC",
+    "name": "クライストチャーチ",
+    "country": "ニュージーランド"
+  },
+  {
+    "code": "CHI",
+    "name": "シカゴ",
+    "country": "アメリカ"
+  },
+  {
+    "code": "ORD",
+    "name": "オヘア国際空港",
+    "country": "シカゴ(アメリカ)"
+  },
+  {
+    "code": "CJU",
+    "name": "済州島",
+    "country": "韓国"
+  },
+  {
+    "code": "CNS",
+    "name": "ケアンズ",
+    "country": "オーストラリア"
+  },
+  {
+    "code": "CNX",
+    "name": "チュンマイ",
+    "country": "タイ"
+  },
+  {
+    "code": "CPH",
+    "name": "コペンハーゲン",
+    "country": "デンマーク"
+  },
+  {
+    "code": "DAD",
+    "name": "ダナン",
+    "country": "ベトナム"
+  },
+  {
+    "code": "DEL",
+    "name": "デリー",
+    "country": "インド"
+  },
+  {
+    "code": "DEN",
+    "name": "デンバー",
+    "country": "アメリカ"
+  },
+  {
+    "code": "DFW",
+    "name": "ダラス/フォートワース",
+    "country": "アメリカ"
+  },
+  {
+    "code": "DLC",
+    "name": "大連",
+    "country": "中国"
+  },
+  {
+    "code": "DPS",
+    "name": "デンパサール",
+    "country": "インドネシア"
+  },
+  {
+    "code": "DTT",
+    "name": "デトロイト",
+    "country": "アメリカ"
+  },
+  {
+    "code": "DUB",
+    "name": "ダブリン",
+    "country": "アイルランド"
+  },
+  {
+    "code": "DUS",
+    "name": "デュッセルドルフ",
+    "country": "ドイツ"
+  },
+  {
+    "code": "DXB",
+    "name": "ドバイ",
+    "country": "アラブ首長国連邦"
+  },
+  {
+    "code": "FRA",
+    "name": "フランクフルト",
+    "country": "ドイツ"
+  },
+  {
+    "code": "FUK",
+    "name": "福岡",
+    "country": "日本"
+  },
+  {
+    "code": "GVA",
+    "name": "ジュネーブ",
+    "country": "スイス"
+  },
+  {
+    "code": "HAM",
+    "name": "ハンブルク",
+    "country": "ドイツ"
+  },
+  {
+    "code": "HAN",
+    "name": "ハノイ",
+    "country": "ベトナム"
+  },
+  {
+    "code": "HAV",
+    "name": "ハバナ",
+    "country": "キューバ"
+  },
+  {
+    "code": "HEL",
+    "name": "ヘルシンキ",
+    "country": "フィンランド"
+  },
+  {
+    "code": "HKG",
+    "name": "香港",
+    "country": "中国"
+  },
+  {
+    "code": "HKT",
+    "name": "プーケット",
+    "country": "タイ"
+  },
+  {
+    "code": "HNL",
+    "name": "ホノルル",
+    "country": "アメリカ・オアフ島"
+  },
+  {
+    "code": "HOU",
+    "name": "ヒューストン",
+    "country": "アメリカ"
+  },
+  {
+    "code": "ISB",
+    "name": "イスラマバード",
+    "country": "パキスタン"
+  },
+  {
+    "code": "IST",
+    "name": "イスタンブール",
+    "country": "トルコ"
+  },
+  {
+    "code": "JKT",
+    "name": "ジャカルタ",
+    "country": "インドネシア"
+  },
+  {
+    "code": "CGK",
+    "name": "スカルノハッタ空港",
+    "country": "ジャカルタ(インドネシア)"
+  },
+  {
+    "code": "JNB",
+    "name": "ヨハネスブルク",
+    "country": "南アフリカ"
+  },
+  {
+    "code": "KHH",
+    "name": "高雄",
+    "country": "台湾"
+  },
+  {
+    "code": "KOA",
+    "name": "コナ",
+    "country": "アメリカ・ハワイ島"
+  },
+  {
+    "code": "KUL",
+    "name": "クアラルンプール",
+    "country": "マレーシア"
+  },
+  {
+    "code": "KWI",
+    "name": "クウェート",
+    "country": "クウェート"
+  },
+  {
+    "code": "KWL",
+    "name": "桂林",
+    "country": "中国"
+  },
+  {
+    "code": "LAS",
+    "name": "ラスベガス",
+    "country": "アメリカ"
+  },
+  {
+    "code": "LAX",
+    "name": "ロサンゼルス",
+    "country": "アメリカ"
+  },
+  {
+    "code": "LED",
+    "name": "サンクト・ペテルブルク",
+    "country": "ロシア"
+  },
+  {
+    "code": "LIS",
+    "name": "リスボン",
+    "country": "ポルトガル"
+  },
+  {
+    "code": "LON",
+    "name": "ロンドン",
+    "country": "イギリス"
+  },
+  {
+    "code": "LHR",
+    "name": "ヒースロー空港",
+    "country": "ロンドン(イギリス)"
+  },
+  {
+    "code": "LGW",
+    "name": "ガトウィック空港",
+    "country": "ロンドン(イギリス)"
+  },
+  {
+    "code": "MAD",
+    "name": "マドリード",
+    "country": "スペイン"
+  },
+  {
+    "code": "MEL",
+    "name": "メルボルン",
+    "country": "オーストラリア"
+  },
+  {
+    "code": "MEX",
+    "name": "メキシコ・シティ",
+    "country": "メキシコ"
+  },
+  {
+    "code": "MIA",
+    "name": "マイアミ",
+    "country": "アメリカ"
+  },
+  {
+    "code": "MIL",
+    "name": "ミラノ",
+    "country": "イタリア"
+  },
+  {
+    "code": "MNL",
+    "name": "マニラ",
+    "country": "フィリピン"
+  },
+  {
+    "code": "MOW",
+    "name": "モスクワ",
+    "country": "ロシア"
+  },
+  {
+    "code": "SVO",
+    "name": "シェレメチェヴォ国際空港",
+    "country": "モスクワ(ロシア)"
+  },
+  {
+    "code": "MRS",
+    "name": "マルセイユ",
+    "country": "フランス"
+  },
+  {
+    "code": "MSY",
+    "name": "ニューオリンズ",
+    "country": "アメリカ"
+  },
+  {
+    "code": "MUC",
+    "name": "ミュンヘン",
+    "country": "ドイツ"
+  },
+  {
+    "code": "NAN",
+    "name": "ナンディ",
+    "country": "フィジー"
+  },
+  {
+    "code": "NAP",
+    "name": "ナポリ",
+    "country": "イタリア"
+  },
+  {
+    "code": "NCE",
+    "name": "ニース",
+    "country": "フランス"
+  },
+  {
+    "code": "NGO",
+    "name": "名古屋",
+    "country": "日本"
+  },
+  {
+    "code": "NYC",
+    "name": "ニューヨーク",
+    "country": "アメリカ"
+  },
+  {
+    "code": "JFK",
+    "name": "ジョン・エフ・ケネディ国際空港",
+    "country": "ニューヨーク(アメリカ)"
+  },
+  {
+    "code": "EWR",
+    "name": "ニューアーク・リバティ国際空港",
+    "country": "ニューヨーク(アメリカ)"
+  },
+  {
+    "code": "LGA",
+    "name": "ラ・ガーディア国際空港",
+    "country": "ニューヨーク(アメリカ)"
+  },
+  {
+    "code": "OOL",
+    "name": "ゴールドコースト",
+    "country": "オーストラリア"
+  },
+  {
+    "code": "ORL",
+    "name": "オーランド",
+    "country": "アメリカ"
+  },
+  {
+    "code": "OSA",
+    "name": "大阪",
+    "country": "日本"
+  },
+  {
+    "code": "KIX",
+    "name": "関西国際空港",
+    "country": "大阪(日本)"
+  },
+  {
+    "code": "ITM",
+    "name": "伊丹空港",
+    "country": "大阪(日本)"
+  },
+  {
+    "code": "OSL",
+    "name": "オスロ",
+    "country": "ノルウェー"
+  },
+  {
+    "code": "PAR",
+    "name": "パリ",
+    "country": "フランス"
+  },
+  {
+    "code": "CDG",
+    "name": "シャルル・ド・ゴール空港",
+    "country": "パリ(フランス)"
+  },
+  {
+    "code": "ORY",
+    "name": "オルリー空港",
+    "country": "パリ(フランス)"
+  },
+  {
+    "code": "PEN",
+    "name": "ペナン",
+    "country": "マレーシア"
+  },
+  {
+    "code": "PER",
+    "name": "パース",
+    "country": "オーストラリア"
+  },
+  {
+    "code": "PHL",
+    "name": "フィラデルフィア",
+    "country": "アメリカ"
+  },
+  {
+    "code": "PNH",
+    "name": "プノンペン",
+    "country": "カンボジア"
+  },
+  {
+    "code": "PRG",
+    "name": "プラハ",
+    "country": "チェコ"
+  },
+  {
+    "code": "PUS",
+    "name": "釜山",
+    "country": "韓国"
+  },
+  {
+    "code": "REP",
+    "name": "シェムリアップ",
+    "country": "カンボジア"
+  },
+  {
+    "code": "RGN",
+    "name": "ヤンゴン",
+    "country": "ミャンマー"
+  },
+  {
+    "code": "RIO",
+    "name": "リオ・デ・ジャネイロ",
+    "country": "ブラジル"
+  },
+  {
+    "code": "ROM",
+    "name": "ローマ",
+    "country": "イタリア"
+  },
+  {
+    "code": "FCO",
+    "name": "フィウミチーノ空港",
+    "country": "ローマ(イタリア)"
+  },
+  {
+    "code": "SAN",
+    "name": "サンディエゴ",
+    "country": "アメリカ"
+  },
+  {
+    "code": "SAO",
+    "name": "サンパウロ",
+    "country": "ブラジル"
+  },
+  {
+    "code": "SEA",
+    "name": "シアトル",
+    "country": "アメリカ"
+  },
+  {
+    "code": "SEL",
+    "name": "ソウル",
+    "country": "韓国"
+  },
+  {
+    "code": "ICN",
+    "name": "仁川国際空港",
+    "country": "ソウル(韓国)"
+  },
+  {
+    "code": "GMP",
+    "name": "金浦国際空港",
+    "country": "ソウル(韓国)"
+  },
+  {
+    "code": "SFO",
+    "name": "サンフランシスコ",
+    "country": "アメリカ"
+  },
+  {
+    "code": "SGN",
+    "name": "ホーチミン・シティ",
+    "country": "ベトナム"
+  },
+  {
+    "code": "SHA",
+    "name": "上海/虹橋国際空港",
+    "country": "中国"
+  },
+  {
+    "code": "PVG",
+    "name": "浦東国際空港",
+    "country": "上海(中国)"
+  },
+  {
+    "code": "SIA",
+    "name": "西安",
+    "country": "中国"
+  },
+  {
+    "code": "XIY",
+    "name": "西安咸陽国際空港",
+    "country": "中国"
+  },
+  {
+    "code": "SIN",
+    "name": "シンガポール",
+    "country": "シンガポール"
+  },
+  {
+    "code": "SJC",
+    "name": "サンノゼ",
+    "country": "アメリカ"
+  },
+  {
+    "code": "SPK",
+    "name": "札幌",
+    "country": "日本"
+  },
+  {
+    "code": "SPN",
+    "name": "サイパン",
+    "country": "北マリアナ諸島"
+  },
+  {
+    "code": "STO",
+    "name": "ストックホルム",
+    "country": "スウェーデン"
+  },
+  {
+    "code": "SVQ",
+    "name": "セビリア",
+    "country": "スペイン"
+  },
+  {
+    "code": "SYD",
+    "name": "シドニー",
+    "country": "オーストラリア"
+  },
+  {
+    "code": "TPE",
+    "name": "台北/桃園国際空港",
+    "country": "台湾"
+  },
+  {
+    "code": "TSA",
+    "name": "松山国際空港",
+    "country": "台北(台湾)"
+  },
+  {
+    "code": "TYO",
+    "name": "東京",
+    "country": "日本"
+  },
+  {
+    "code": "NRT",
+    "name": "成田国際空港",
+    "country": "東京(日本)"
+  },
+  {
+    "code": "HND",
+    "name": "羽田空港",
+    "country": "東京(日本)"
+  },
+  {
+    "code": "VCE",
+    "name": "ベニス",
+    "country": "イタリア"
+  },
+  {
+    "code": "VIE",
+    "name": "ウィーン",
+    "country": "オーストリア"
+  },
+  {
+    "code": "WAS",
+    "name": "ワシントンDC",
+    "country": "アメリカ"
+  },
+  {
+    "code": "IAD",
+    "name": "ダレス国際空港",
+    "country": "ワシントンDC(アメリカ)"
+  },
+  {
+    "code": "DCA",
+    "name": "ロナルド・レーガン・ナショナル空港",
+    "country": "ワシントンDC(アメリカ)"
+  },
+  {
+    "code": "WAW",
+    "name": "ワルシャワ",
+    "country": "ポーランド"
+  },
+  {
+    "code": "WLG",
+    "name": "ウェリントン",
+    "country": "ニュージーランド"
+  },
+  {
+    "code": "YEG",
+    "name": "エドモントン",
+    "country": "カナダ"
+  },
+  {
+    "code": "YMQ",
+    "name": "モントリオール",
+    "country": "カナダ"
+  },
+  {
+    "code": "YQB",
+    "name": "ケベック・シティ",
+    "country": "カナダ"
+  },
+  {
+    "code": "YTO",
+    "name": "トロント",
+    "country": "カナダ"
+  },
+  {
+    "code": "YYZ",
+    "name": "レスター・B・ピアソン国際空港",
+    "country": "トロント(カナダ)"
+  },
+  {
+    "code": "YVR",
+    "name": "バンクーバー",
+    "country": "カナダ"
+  },
+  {
+    "code": "YYC",
+    "name": "カルガリー",
+    "country": "カナダ"
+  },
+  {
+    "code": "YYJ",
+    "name": "ビクトリア",
+    "country": "カナダ"
+  },
+  {
+    "code": "ZRH",
+    "name": "チューリヒ",
+    "country": "スイス"
+  }
+];
